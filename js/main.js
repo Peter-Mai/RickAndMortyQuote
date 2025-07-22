72f27e774595bb20e1b12647f246fc98a939ac05
Peter Mai <peter.mai1996@gmail.com>
2025-07-22 14:09:52 -0500

    Added a number variable to randomize characters being called. Template literal for url endpoint

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const url = 'https://rickandmortyapi.com/api/character/2'
-
+  const number = Math.floor(Math.random() * 100);
+  const url = `https://rickandmortyapi.com/api/character/${number}`
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+        console.log(data)
         document.querySelector('h2').innerText = data.name
       })
       .catch(err => {
