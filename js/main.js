3d453e4bd5b81ca309c14ccaa9175368a78229ac
Peter Mai <peter.mai1996@gmail.com>
2025-07-22 14:01:08 -0500

    Removed a variable and change the endpoint to point towards grabbing characters from Rick and Morty

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = 'https://rickandmortyapi.com/api/character/2'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
